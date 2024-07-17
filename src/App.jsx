e334907798b6d8a99bd9e518798db5d224302d6f
amitjhajhria0100 <amitjhajhria732@gmail.com>
2024-07-17 09:52:54 +0530

    signup and signin popup ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import {Routes , Route} from 'react-router-dom'
 import Landing from './pages/Landing'
+import SignInPopUp from './LoginSignup/SignInPopUp'
+import SignUpPopUp from './SignUp/SignUpPopUp'
 
 function App() {
 
@@ -9,6 +11,8 @@ function App() {
      <Routes>
       <Route path='/' element={<Landing/>}/>
      </Routes>
+     {/* <SignInPopUp/> */}
+     {/* <SignUpPopUp/> */}
     </>
   )
 }
